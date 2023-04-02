7e69bda9682187737a48303141fe4ac750eb6619
Nanatsu2370 <94955661+Nanatsu2370@users.noreply.github.com>
2023-04-02 18:47:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        #tl_admin_user = 'obidevel'
+        tl_admin_user = 'Nanatsu2370'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
